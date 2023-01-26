f6cd2fa3964d24be43a2033188d048b4defefe5b
Shrief Essam <shriefessam1999@gmail.com>
2023-01-27 00:00:25 +0200

    add caching for images

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import sharp from "sharp";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
@@ -12,30 +13,36 @@ interface Query {
   height: string;
 }
 
-const resizeImage = async (
-  req: express.Request,
-  res: express.Response,
-  next: () => void
-): Promise<void> => {
+const resizeImage = async (req: express.Request): Promise<void> => {
   const { fillname, width, height } = req.query as unknown as Query;
-  const filePath = path.resolve(__dirname, "..", "full", `${fillname}`);
-  const outputPath = path.resolve(__dirname, "..", "thumb", `${fillname}`);
+  const filePath = path.resolve(__dirname, "..", "full", `${fillname}.jpg`);
+  const outputPath = path.resolve(
+    __dirname,
+    "..",
+    "thumb",
+    `${fillname}-${width}-${height}.jpg`
+  );
 
   await sharp(filePath)
     .resize({ width: parseInt(width), height: parseInt(height) })
     .toFile(outputPath);
-  next();
 };
 
-app.get(
-  "/api/images",
-  resizeImage,
-  (req: express.Request, res: express.Response) => {
-    const { fillname } = req.query as unknown as Query;
-    const filePath = path.resolve(__dirname, "..", "thumb", `${fillname}`);
-    res.sendFile(filePath);
+app.get("/api/images", async (req: express.Request, res: express.Response) => {
+  const { fillname, width, height } = req.query as unknown as Query;
+  const filePath = path.resolve(
+    __dirname,
+    "..",
+    "thumb",
+    `${fillname}-${width}-${height}.jpg`
+  );
+
+  if (!fs.existsSync(filePath)) {
+    await resizeImage(req);
   }
-);
+
+  res.sendFile(filePath);
+});
 
 app.listen(port, hostname, () => {
   console.log(`server is running on http://${hostname}:${port}`);
