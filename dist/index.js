d9c5435d06a6968f32b14af56df28f0a186aa87b
Shrief Essam <shriefessam1999@gmail.com>
2023-01-26 18:03:22 +0200

    make sure the server is running

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var port = 3000;
+var hostname = 'localhost';
+app.listen(port, hostname, function () {
+    console.log("server is running on ".concat(hostname, ":").concat(port));
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+import express from "express";
+
+const app = express();
+const port = 3000;
+const hostname = "localhost";
+
+app.listen(port, hostname, () => {
+  console.log(`server is running on http://${hostname}:${port}`);
+});
