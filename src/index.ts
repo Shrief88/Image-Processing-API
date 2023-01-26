66b534aa5c6edbb69056e4eba57b27fe1fa8c37a
Shrief Essam <shriefessam1999@gmail.com>
2023-01-26 19:33:21 +0200

    create endpoint to send image as response

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = 3000;
 const hostname = "localhost";
+
+app.get("/api/images", (req: express.Request, res: express.Response) => {
+  const filePath = path.resolve(
+    __dirname,
+    "..",
+    "full",
+    `${req.query.fillname}`
+  );
+  res.sendFile(filePath);
+});
 
 app.listen(port, hostname, () => {
   console.log(`server is running on http://${hostname}:${port}`);
